36b5a12e26662edd8917ec3e71cf9369f8dff76a
Ekho <ekho@ekho.email>
2018-11-25 18:34:21 +0000

    update license

--- mediadir.pl
+++ mediadir.pl
@@ -4,7 +4,7 @@
 =pod LICENSE
 
  BSD-0
- Copyright (C) 2018, Ekho <ekho@ekho,email> \ EkhoNet::Ag_Labs
+ Copyright (C) 2018, Ekho <ekho@ekho,email>
 
  Permission to use, copy, modify, and/or distribute this software for any
  purpose with or without fee is hereby granted.
